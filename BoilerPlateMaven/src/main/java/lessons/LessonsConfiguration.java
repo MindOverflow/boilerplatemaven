d22f75b2da0c1b5c337fb7f691547cd254043982
Коваленко Антон <Kovalenko.A.Andr@sberbank.ru>
2020-04-14 16:39:26 +0300

    Пример. Простое подключение конфигурации и контекста

---
+++ BoilerPlateMaven/src/main/java/lessons/LessonsConfiguration.java
@@ -0,0 +1,19 @@
+package lessons;
+
+import lessons.services.GreetingService;
+import lessons.services.GreetingServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+
+/**
+ * Конфигурационный класс Spring IoC Container'a
+ */
+@Configuration
+public class LessonsConfiguration {
+    @Bean
+    @Description("Текстовое описание бина greetingService")
+    GreetingService greetingService() {
+        return new GreetingServiceImpl();
+    }
+}
--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
@@ -1,12 +1,26 @@
 package ru.sberbank.mavenboilerplateapp;
 
+
+import lessons.LessonsConfiguration;
+import lessons.services.GreetingService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
- * Hello world!
- *
+ * Полное описание примера:
+ * https://github.com/wertklop/spring-lessons.git
+ * https://spring-projects.ru/guides/lessons/lesson-2/
  */
 public class App 
 {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static final Logger logger = LogManager.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.info("Старт конфигурации");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(LessonsConfiguration.class);
+        GreetingService greetingService = context.getBean(GreetingService.class);
+        logger.info(greetingService.sayGreeting());
     }
 }
