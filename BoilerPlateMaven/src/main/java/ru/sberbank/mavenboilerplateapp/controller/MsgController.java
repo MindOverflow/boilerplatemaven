32273bc73f9b2438fe63f3e82d7bbb0b2920d0b3
Коваленко Антон <Kovalenko.A.Andr@sberbank.ru>
2020-05-02 22:08:42 +0300

    Пост на HabraHabr
    Добавлена возможность сбрасывать в консоль результат продюсера

--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/controller/MsgController.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/controller/MsgController.java
@@ -1,11 +1,10 @@
 package ru.sberbank.mavenboilerplateapp.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +22,19 @@ public class MsgController {
 
     /**
      * @method
+     *     отправляет полученны параметры с клиента в сообщение с ключом и текстом сообещения
      * @param msgId - ключ сообщения
      * @param msg - сообщение, которое публикует издатель
      */
     @PostMapping
-    public void sendOrder(String msgId, String msg) {
+    public void sendMessage(String msgId, String msg) {
         log.info("Идентификатор сообщения: " + msgId);
         log.info("Сообщение с клиента: " + msg);
+
         // "msg" - название топика, который может не существовать. Если не существует, создаётся автоматом.
-        kafkaTemplate.send("msg", msgId, msg);
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("msg", msgId, msg);
+
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
     }
 }
