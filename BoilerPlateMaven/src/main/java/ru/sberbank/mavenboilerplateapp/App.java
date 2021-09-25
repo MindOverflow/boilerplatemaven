709e4eebbca73ac735cef97f2245e9fa7f6928c1
Коваленко Антон <Kovalenko.A.Andr@sberbank.ru>
2021-09-26 01:59:13 +0300

    Стартовый вариант листинга 05. 07.

--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
@@ -2,6 +2,12 @@ package ru.sberbank.mavenboilerplateapp;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
 /**
  * Hello World my dear Slf4j!
  */
@@ -9,5 +15,72 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
     public static void main(String[] args) {
         log.info("Hello World!");
+        ArrayList<Integer> squares = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            squares.add(i * i);
+        }
+        System.out.println(new ObjectAnalyser().toString(squares));
     }
+}
+
+class ObjectAnalyser {
+    public String toString(Object obj) {
+        if (obj == null) {
+            return "null";
+        }
+        if (visited.contains(obj)) {
+            return "...";
+        }
+        visited.add(obj);
+        Class cl = obj.getClass();
+        if (cl == String.class) {
+            return (String)obj;
+        }
+        if (cl.isArray()) {
+            String r = cl.getComponentType() + "[]{";
+            for (int i = 0; i < Array.getLength(obj); i++) {
+                if (i > 0) r += ",";
+                Object val = Array.get(obj, i);
+                if (cl.getComponentType().isPrimitive()) {
+                    r += val;
+                } else  {
+                    r += toString(val);
+                }
+            }
+            return r + "}";
+        }
+        String r = cl.getName();
+        // Проверка полей класса и всех его суперклассов.
+        do {
+            r += "[";
+            Field[] fields = cl.getDeclaredFields();
+            AccessibleObject.setAccessible(fields, true);
+            // Определение имён и значений всех полей.
+            for (Field f : fields) {
+                if (!Modifier.isStatic(f.getModifiers())) {
+                    if (!r.endsWith("[")) {
+                        r += ",";
+                    }
+                    r += f.getName() + "=";
+                    try {
+                        Class t = f.getType();
+                        Object val = f.get(obj);
+                        if (t.isPrimitive()) {
+                            r += val;
+                        } else {
+                            r += toString(val);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            r += "]";
+            cl = cl.getSuperclass();
+        } while (cl != null);
+
+        return r;
+    }
+
+    private ArrayList<Object> visited = new ArrayList<>();
 }
