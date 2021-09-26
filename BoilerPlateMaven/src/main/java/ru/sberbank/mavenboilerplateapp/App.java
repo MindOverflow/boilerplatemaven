100a41a4399eef3dee089f1166ddb8b0550328f6
MindOverflow <toxaSmith@yahoo.com>
2021-09-27 00:41:22 +0300

    Исходное упражнение листинга 12.01

--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
@@ -2,12 +2,32 @@ package ru.sberbank.mavenboilerplateapp;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Hello World my dear Slf4j!
- */
 @Slf4j
 public class App {
     public static void main(String[] args) {
         log.info("Hello World!");
+        String[] words = { "Mary", "had", "a", "little", "lamb" };
+        Pair<String> mm = ArrayAlg.minmax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+    }
+}
+
+class ArrayAlg {
+    public static Pair<String> minmax(String[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+        String min = a[0];
+        String max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) {
+                min = a[i];
+            }
+            if (max.compareTo(a[i]) < 0) {
+                max = a[i];
+            }
+        }
+        return new Pair<String>(min, max);
     }
 }
