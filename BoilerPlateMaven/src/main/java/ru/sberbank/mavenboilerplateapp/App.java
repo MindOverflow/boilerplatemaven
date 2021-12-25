3fa828ad367fbd4f98ace65ca0a9d68e69162a79
Alexandra <228sand228@gmail.com>
2021-12-26 00:57:24 +0300

    добавлен считыватель xml книг

--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
@@ -16,34 +16,13 @@ public class App
 {
     public static void main(String[] args) throws JAXBException, FileNotFoundException {
         log.info("Starting...");
-        List<Employee> employeeList = new ArrayList<Employee>();
-
-        Employee _Anton = new Employee();
-        _Anton.setId(1);
-        _Anton.setLastName("Коваленко");
-        _Anton.setFirstName("Антон");
-        _Anton.setIncome(100d);
-        employeeList.add(_Anton);
-
-        Employee _Alexandra = new Employee();
-        _Alexandra.setId(2);
-        _Alexandra.setLastName("Рыбцова");
-        _Alexandra.setFirstName("Александра");
-        _Alexandra.setIncome(100d);
-        employeeList.add(_Alexandra);
-        Employees employees = new Employees();
-        employees.setEmployees(employeeList);
-
-        JAXBContext context = JAXBContext.newInstance(Employees.class);
+        // JAXBContext представляет как бы клиентскую входную точку для JAXB API
+        // По умолчанию, JAXB не форматирует XML-документ
+        JAXBContext context = JAXBContext.newInstance(Catalog.class);
         Marshaller marshaller = context.createMarshaller();
-        // Для того чтобы вывод документа был форматированным, мы устанавливаем свойство
-        // Marshaller.JAXB_FORMATTED_OUTPUT равным TRUE
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(employees, System.out);
-        // Когда мы запустим код, мы можем проверить результат в файле book.xml
-        marshaller.marshal(employees, new File("./employees.xml"));
-        Employees employeesRead = (Employees) context.createUnmarshaller().unmarshal(new FileReader("./employees.xml"));
-        log.info(employeesRead.toString());
+
+        Catalog catalog = (Catalog) context.createUnmarshaller().unmarshal(new FileReader("./books.xml"));
+        log.info(catalog.toString());
 
         log.info("The end");
     }
