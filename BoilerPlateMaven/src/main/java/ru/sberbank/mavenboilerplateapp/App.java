5c8f1151cc7c15a74558f81c4fb1e51e913883e0
Alexandra <228sand228@gmail.com>
2021-12-26 02:53:09 +0300

    база данных теперь принимает books.xml и возвращает perst xml

--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
@@ -1,11 +1,12 @@
 package ru.sberbank.mavenboilerplateapp;
 
 import lombok.extern.slf4j.Slf4j;
+import org.garret.perst.Storage;
+import org.garret.perst.StorageFactory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 
 @Slf4j
 public class App
@@ -19,6 +20,48 @@ public class App
         Catalog catalog = (Catalog) context.createUnmarshaller().unmarshal(new FileReader("./books.xml"));
         log.info(catalog.toString());
 
+        Storage db = StorageFactory.getInstance().createStorage();
+        db.open("my_pretty_perst_db.dbs");
+        Catalog root = (Catalog)db.getRoot();
+        if (root == null) {
+            // Root is not yet defined: storage not initialized
+            root = new Catalog(db); // create root object
+            db.setRoot(root); // register root object
+        }
+        int books_count = catalog.getBooks().size();
+        for (int i = 0; i < books_count; i++) {
+            Book book = new Book();
+            Book input_book = catalog.getBooks().get(i);
+
+            book.setId(input_book.getId());
+            book.setAuthor(input_book.getAuthor());
+            book.setTitle(input_book.getTitle());
+            book.setGenre(input_book.getGenre());
+            book.setPrice(input_book.getPrice());
+            book.setPublish_date(input_book.getPublish_date());
+            book.setDescription(input_book.getDescription());
+
+            root.idIndex.put(book);
+            root.authorIndex.put(book);
+            root.titleIndex.put(book);
+            root.genreIndex.put(book);
+            root.priceIndex.put(book);
+            root.publish_dateIndex.put(book);
+            root.descriptionIndex.put(book);
+        }
+        String uploadPath = "./dbexport.xml";
+        try {
+            Writer writer = new BufferedWriter(new FileWriter(uploadPath));
+            // export the whole database to the specified writer in XML
+            // format
+            db.exportXML(writer);
+            // exportXML doesn't close the stream, close it here
+            writer.close();
+        } catch (IOException x) {
+            System.err.println("Export failed: " + x);
+        }
+
+        db.close();
         log.info("The end");
     }
 }
--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/Catalog.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/Catalog.java
@@ -1,7 +1,9 @@
 package ru.sberbank.mavenboilerplateapp;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.garret.perst.FieldIndex;
 import org.garret.perst.Persistent;
+import org.garret.perst.Storage;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -14,11 +16,37 @@ import java.util.List;
 public class Catalog extends Persistent {
     @XmlElement(name = "book")
     private List<Book> books = null;
+
+    public FieldIndex<Book> idIndex;
+    public FieldIndex<Book> authorIndex;
+    public FieldIndex<Book> titleIndex;
+    public FieldIndex<Book> genreIndex;
+    public FieldIndex<Book> priceIndex;
+    public FieldIndex<Book> publish_dateIndex;
+    public FieldIndex<Book> descriptionIndex;
+
     public Catalog() {
 
     }
     public Catalog(List<Book> books) {
         this.books = books;
+    }
+    public Catalog(Storage db) {
+        super(db);
+        idIndex =
+            db.<Book>createFieldIndex(Book.class, "id", true);
+        authorIndex =
+            db.<Book>createFieldIndex(Book.class, "author", false);
+        titleIndex =
+            db.<Book>createFieldIndex(Book.class, "title", false);
+        genreIndex =
+            db.<Book>createFieldIndex(Book.class, "genre", false);
+        priceIndex =
+            db.<Book>createFieldIndex(Book.class, "price", false);
+        publish_dateIndex =
+            db.<Book>createFieldIndex(Book.class, "publish_date", false);
+        descriptionIndex =
+            db.<Book>createFieldIndex(Book.class, "description", false);
     }
     public List<Book> getBooks() {
         return books;
