dbf38c49cf10015f11c7daaa93faee504c6f812d
Коваленко Антон Андреевич <Kovalenko.A.Andr@sberbank.ru>
2021-12-25 16:50:11 +0300

    Пример - как делать чтение XML в объект JAVA с помощью JAXB

--- BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
+++ BoilerPlateMaven/src/main/java/ru/sberbank/mavenboilerplateapp/App.java
@@ -5,28 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import java.io.File;
-import java.util.Date;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 @Slf4j
 public class App
 {
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
         log.info("Starting...");
-        Book book = new Book();
-        book.setId(1L);
-        book.setName("Book1");
-        book.setDate(new Date());
 
         // JAXBContext представляет как бы клиентскую входную точку для JAXB API
         // По умолчанию, JAXB не форматирует XML-документ
         JAXBContext context = JAXBContext.newInstance(Book.class);
         Marshaller marshaller = context.createMarshaller();
-        // Для того чтобы вывод вокумента был форматированным, мы устанавливаем свойство
-        // Marshaller.JAXB_FORMATTED_OUTPUT равным TRUE
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        // Когда мы запустим код, мы можем проверить результат в файле book.xml
-        marshaller.marshal(book, new File("./books.xml"));
+
+        Book book = (Book) context.createUnmarshaller().unmarshal(new FileReader("./books.xml"));
+        log.info(book.toString());
         log.info("The end");
     }
 }
